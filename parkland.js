be00d0a4c76d0dd4a964bb7ccd636c0dce59f967
GustavoHey <107442918+GustavoHey@users.noreply.github.com>
2024-06-23 01:18:30 -0300

    Create parkland.js

---
+++ parkland.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         PARKLAND LONDRINA CAIXA
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  CONEXÃO PRINCIPAL PARKLAND
+// @author       Gustavo Henrique Cirillo
+// @match        *://*.facebook.com/*
+// @match        *://*.twitter.com/*
+// @match        *://*.x.com/*
+// @match        *://*.instagram.com/*
+// @match        *://*.tiktok.com/*
+// @match        *://*.snapchat.com/*
+// @match        *://*.pinterest.com/*
+// @match        *://*.tumblr.com/*
+// @match        *://*.whatsapp.com/*
+// @match        *://*.wechat.com/*
+// @match        *://*.qq.com/*
+// @match        *://*.telegram.org/*
+// @match        *://*.messenger.com/*
+// @match        *://*.vkontakte.ru/*
+// @match        *://*.ok.ru/*
+// @match        *://*.douban.com/*
+// @match        *://*.renren.com/*
+// @match        *://*.mixi.jp/*
+// @match        *://*.weibo.com/*
+// @match        *://*.bilibili.com/*
+// @match        *://*.quora.com/*
+// @match        *://*.flickr.com/*
+// @match        *://*.meetup.com/*
+// @match        *://*.deviantart.com/*
+// @match        *://*.imgur.com/*
+// @match        *://*.vk.com/*
+// @match        *://*.livejournal.com/*
+// @match        *://*.clubhouse.com/*
+// @match        *://*.byte.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Redirect to a warning page or a blank page
+    window.location.href = 'https://gustavohey.github.io/parklandlondr/';
+})();
